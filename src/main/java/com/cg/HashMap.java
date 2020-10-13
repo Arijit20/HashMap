d061a50dde6cdde2ed6d6fea1e61cecf7a88d252
Arijit20 <ARIJIT DEY@LAPTOP-F4J8SDAU>
2020-10-13 20:47:08 +0530

    [Arijit]Added : Finding Frequency Of Word Using HashMap

--- src/main/java/com/cg/HashMap.java
+++ src/main/java/com/cg/HashMap.java
@@ -1,7 +1,29 @@
 package com.cg;
 
-public class HashMap {
-  public static void main(String[] args) {
-	System.out.println("Starting Hash Map");
-}
+public class HashMap<K, V> {
+	LinkedList<K> linkedList;
+
+	public HashMap() {
+		this.linkedList = new LinkedList<>();
+	}
+
+	public V get(K key) {
+		MapNode<K, V> mapNode =  (MapNode<K, V>) this.linkedList.searchNode(key);
+		return (mapNode == null) ? null : mapNode.getValue();
+	}
+
+	public void add(K key, V value) {
+		MapNode<K, V> mapNode = (MapNode<K, V>) linkedList.searchNode(key);
+		if (mapNode == null) {
+			mapNode = new MapNode<>(key, value);
+			linkedList.append(mapNode);
+		} else {
+			mapNode.setValue(value);
+		}
+	}
+
+	@Override
+	public String toString() {
+		return "HashMapNodes{ " + linkedList + "}";
+	}
 }
