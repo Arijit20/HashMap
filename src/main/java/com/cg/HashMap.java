97544e5468c3e20697503b1f63ac9c332a49315a
Arijit20 <ARIJIT DEY@LAPTOP-F4J8SDAU>
2020-10-13 20:39:04 +0530

    [Arijit]Added : Starting Hash Map

---
+++ src/main/java/com/cg/HashMap.java
@@ -0,0 +1,7 @@
+package com.cg;
+
+public class HashMap {
+  public static void main(String[] args) {
+	System.out.println("Starting Hash Map");
+}
+}
---
+++ src/test/java/com/cg/HashMapTest.java
@@ -0,0 +1,12 @@
+
+package com.cg;
+
+import org.junit.Test;
+
+import com.cg.Node;
+
+import static org.junit.Assert.*;
+
+public class HashMapTest {
+   
+}
